25c7be97f91f4743af908116020773a22d9dd7eb
Luke Saufley <Histalems18@outlook.com>
2021-07-17 00:22:49 -0400

    Added ProfileDialog

---
+++ PersonDetails.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace MultiFaceRec
+{
+    partial class PersonDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picProfile = new System.Windows.Forms.PictureBox();
+            this.lblFirst = new System.Windows.Forms.Label();
+            this.txtFirst = new System.Windows.Forms.TextBox();
+            this.txtLast = new System.Windows.Forms.TextBox();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.txtMiddle = new System.Windows.Forms.TextBox();
+            this.lblMiddleName = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.lblRequired = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picProfile)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picProfile
+            // 
+            this.picProfile.Location = new System.Drawing.Point(321, 13);
+            this.picProfile.Margin = new System.Windows.Forms.Padding(4);
+            this.picProfile.Name = "picProfile";
+            this.picProfile.Size = new System.Drawing.Size(100, 100);
+            this.picProfile.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picProfile.TabIndex = 0;
+            this.picProfile.TabStop = false;
+            // 
+            // lblFirst
+            // 
+            this.lblFirst.AutoSize = true;
+            this.lblFirst.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirst.Location = new System.Drawing.Point(15, 18);
+            this.lblFirst.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFirst.Name = "lblFirst";
+            this.lblFirst.Size = new System.Drawing.Size(85, 17);
+            this.lblFirst.TabIndex = 1;
+            this.lblFirst.Text = "First Name *";
+            // 
+            // txtFirst
+            // 
+            this.txtFirst.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirst.Location = new System.Drawing.Point(101, 15);
+            this.txtFirst.Margin = new System.Windows.Forms.Padding(4);
+            this.txtFirst.Name = "txtFirst";
+            this.txtFirst.Size = new System.Drawing.Size(211, 23);
+            this.txtFirst.TabIndex = 2;
+            this.txtFirst.Tag = "First Name";
+            // 
+            // txtLast
+            // 
+            this.txtLast.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLast.Location = new System.Drawing.Point(99, 90);
+            this.txtLast.Margin = new System.Windows.Forms.Padding(4);
+            this.txtLast.Name = "txtLast";
+            this.txtLast.Size = new System.Drawing.Size(213, 23);
+            this.txtLast.TabIndex = 6;
+            this.txtLast.Tag = "Last Name";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastName.Location = new System.Drawing.Point(15, 93);
+            this.lblLastName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(76, 17);
+            this.lblLastName.TabIndex = 5;
+            this.lblLastName.Text = "Last Name";
+            // 
+            // txtMiddle
+            // 
+            this.txtMiddle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMiddle.Location = new System.Drawing.Point(108, 55);
+            this.txtMiddle.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMiddle.Name = "txtMiddle";
+            this.txtMiddle.Size = new System.Drawing.Size(204, 23);
+            this.txtMiddle.TabIndex = 4;
+            this.txtMiddle.Tag = "Middle Name";
+            // 
+            // lblMiddleName
+            // 
+            this.lblMiddleName.AutoSize = true;
+            this.lblMiddleName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMiddleName.Location = new System.Drawing.Point(15, 58);
+            this.lblMiddleName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMiddleName.Name = "lblMiddleName";
+            this.lblMiddleName.Size = new System.Drawing.Size(90, 17);
+            this.lblMiddleName.TabIndex = 3;
+            this.lblMiddleName.Text = "Middle Name";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(321, 371);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(100, 28);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "&Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(212, 371);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(4);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(100, 28);
+            this.btnOK.TabIndex = 7;
+            this.btnOK.Text = "&OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // lblRequired
+            // 
+            this.lblRequired.AutoSize = true;
+            this.lblRequired.Location = new System.Drawing.Point(13, 378);
+            this.lblRequired.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblRequired.Name = "lblRequired";
+            this.lblRequired.Size = new System.Drawing.Size(128, 17);
+            this.lblRequired.TabIndex = 9;
+            this.lblRequired.Text = "* = Required Fields";
+            // 
+            // PersonDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 412);
+            this.Controls.Add(this.lblRequired);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.txtMiddle);
+            this.Controls.Add(this.lblMiddleName);
+            this.Controls.Add(this.txtLast);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.txtFirst);
+            this.Controls.Add(this.lblFirst);
+            this.Controls.Add(this.picProfile);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "PersonDetails";
+            this.Text = "PersonDetails";
+            ((System.ComponentModel.ISupportInitialize)(this.picProfile)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picProfile;
+        private System.Windows.Forms.Label lblFirst;
+        private System.Windows.Forms.TextBox txtFirst;
+        private System.Windows.Forms.TextBox txtLast;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.TextBox txtMiddle;
+        private System.Windows.Forms.Label lblMiddleName;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label lblRequired;
+    }
+}
---
+++ PersonDetails.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiFaceRec
+{
+    public partial class PersonDetails : Form
+    {
+        int profileIndex=0;
+        String dbLoc = Environment.ExpandEnvironmentVariables("%appdata%") + @"\LeMS\Imagenation\DB\";
+        public PersonDetails(System.Drawing.Image profile)
+        {
+            InitializeComponent();
+            picProfile.Image = profile;
+        }
+        public PersonDetails(int index)
+        {
+            profileIndex = index;
+        }
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (Required(txtFirst))
+            {
+                while (profileIndex == 0 || Directory.Exists(dbLoc + profileIndex.ToString("000000"))) profileIndex++;
+                Directory.CreateDirectory(dbLoc+ profileIndex.ToString("000000"));
+                libProChic.ConfigHelper profile = new libProChic.ConfigHelper(dbLoc + profileIndex.ToString("000000") + @"\details.pro",false);
+                if (!File.Exists(dbLoc + profileIndex.ToString("000000") + @"\pro.jpg")) picProfile.Image.Save(dbLoc + profileIndex.ToString("000000") + @"\pro.jpg", System.Drawing.Imaging.ImageFormat.Bmp);
+                profile.SetConfig("Hidden", "Ver", Application.ProductVersion,true);
+                profile.SetConfig("Main", "FN", txtFirst.Text,true);
+                profile.SetConfig("Main", "MN", txtMiddle.Text,true);
+                profile.SetConfig("Main", "LN", txtLast.Text,true);
+                profile.Save();
+                DialogResult = DialogResult.OK;
+                this.Close();
+            }
+        }
+        private Boolean Required(TextBox txt)
+        {
+            if (txt.Text == "")
+            {
+                MessageBox.Show("Please fill out the required field " + txt.Tag.ToString());
+                txt.Focus();
+                return false;
+            }
+            return true;
+        }
+        public String ProfileName()
+        {
+            return (txtFirst.Text + " " + txtLast.Text).Trim();
+        }
+        public String profileLocation()
+        {
+            return dbLoc + profileIndex.ToString("000000");
+        }
+    }
+}
