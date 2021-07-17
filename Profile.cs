a322ff242f622d562da182357db297eee607fbb6
Luke Saufley <Histalems18@outlook.com>
2021-07-17 00:23:29 -0400

    Added basic Profile (will be greatly improved in future)

---
+++ Profile.cs
@@ -0,0 +1,27 @@
+﻿using libProChic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiFaceRec
+{
+    public class Profile
+    {
+        private ConfigHelper prof;
+        public Profile()
+        {
+
+        }
+
+        public Profile(String fileName)
+        {
+            prof = new ConfigHelper(fileName);
+        }
+        public String FirstLastName()
+        {
+            return prof.GetConfig("Main", "FN").Setting + " " + prof.GetConfig("Main", "LN").Setting;
+        }
+    }
+}
