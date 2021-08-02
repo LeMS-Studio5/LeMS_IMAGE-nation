6cdb7af3682446026ef7e46f8a8f48f78539fe10
Luke Saufley <Histalems18@outlook.com>
2021-08-02 00:36:12 -0400

    Added Directory Manager

---
+++ DirectoryManager.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace MultiFaceRec
+{
+    partial class DirectoryManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fbdDirMan = new System.Windows.Forms.FolderBrowserDialog();
+            this.lbxDirs = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // fbdDirMan
+            // 
+            this.fbdDirMan.ShowNewFolderButton = false;
+            // 
+            // lbxDirs
+            // 
+            this.lbxDirs.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxDirs.FormattingEnabled = true;
+            this.lbxDirs.ItemHeight = 16;
+            this.lbxDirs.Location = new System.Drawing.Point(12, 12);
+            this.lbxDirs.Name = "lbxDirs";
+            this.lbxDirs.Size = new System.Drawing.Size(535, 276);
+            this.lbxDirs.TabIndex = 0;
+            this.lbxDirs.SelectedIndexChanged += new System.EventHandler(this.lbxDirs_SelectedIndexChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(12, 320);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(182, 31);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "&Add Folder";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Enabled = false;
+            this.btnDel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDel.Location = new System.Drawing.Point(365, 320);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(182, 31);
+            this.btnDel.TabIndex = 2;
+            this.btnDel.Text = "&Delete Folder";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // DirectoryManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 363);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lbxDirs);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "DirectoryManager";
+            this.Text = "DirectoryManager";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog fbdDirMan;
+        private System.Windows.Forms.ListBox lbxDirs;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDel;
+    }
+}
---
+++ DirectoryManager.cs
@@ -0,0 +1,44 @@
+﻿using libProChic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiFaceRec
+{
+    public partial class DirectoryManager : Form
+    {
+        ConfigHelper con;
+        public DirectoryManager(ConfigHelper config)
+        {
+            InitializeComponent();
+            con = config;
+        }
+
+        private void lbxDirs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnDel.Enabled = (lbxDirs.SelectedIndex > -1);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (fbdDirMan.ShowDialog() == DialogResult.OK)
+            {
+                lbxDirs.Items.Add(fbdDirMan.SelectedPath);
+                con.SetConfig("Dir", con.GetConfigGroup("Dir").ToArray().Length+"", fbdDirMan.SelectedPath,true);
+                con.Save();
+            }
+        }
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            con.GetConfigGroup("Dir").RemoveAt(lbxDirs.SelectedIndex);
+            lbxDirs.Items.RemoveAt(lbxDirs.SelectedIndex);
+            con.Save();
+        }
+    }
+}
